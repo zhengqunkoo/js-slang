66a090b4cf717029dc421eb113fe24677911d490
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2018-12-15 06:10:43 +0800

    Move runInContext and friends to runner.ts

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,7 @@
-import createContext from './createContext'
-import { evaluate } from './interpreter'
-import { InterruptedError } from './interpreter-errors'
-import { parse } from './parser'
-import { AsyncScheduler, PreemptiveScheduler } from './schedulers'
-import { Context, Error, Finished, Result, Scheduler, SourceError } from './types'
-
-export interface IOptions {
-  scheduler: 'preemptive' | 'async'
-  steps: number
-}
-
-const DEFAULT_OPTIONS: IOptions = {
-  scheduler: 'async',
-  steps: 1000
-}
+export { default as createContext } from './createContext'
+export { Context, Result } from './types'
+export * from './runner'
+import { SourceError } from './types'
 
 export function parseError(errors: SourceError[]): string {
   const errorMessagesArr = errors.map(error => {
@@ -23,42 +11,3 @@ export function parseError(errors: SourceError[]): string {
   })
   return errorMessagesArr.join('\n')
 }
-
-export function runInContext(
-  code: string,
-  context: Context,
-  options: Partial<IOptions> = {}
-): Promise<Result> {
-  const theOptions: IOptions = { ...DEFAULT_OPTIONS, ...options }
-  context.errors = []
-  const program = parse(code, context)
-  if (program) {
-    const it = evaluate(program, context)
-    let scheduler: Scheduler
-    if (theOptions.scheduler === 'async') {
-      scheduler = new AsyncScheduler()
-    } else {
-      scheduler = new PreemptiveScheduler(theOptions.steps)
-    }
-    return scheduler.run(it, context)
-  } else {
-    return Promise.resolve({ status: 'error' } as Result)
-  }
-}
-
-export function resume(result: Result): Finished | Error | Promise<Result> {
-  if (result.status === 'finished' || result.status === 'error') {
-    return result
-  } else {
-    return result.scheduler.run(result.it, result.context)
-  }
-}
-
-export function interrupt(context: Context) {
-  const globalFrame = context.runtime.frames[context.runtime.frames.length - 1]
-  context.runtime.frames = [globalFrame]
-  context.runtime.isRunning = false
-  context.errors.push(new InterruptedError(context.runtime.nodes[0]))
-}
-
-export { createContext, Context, Result }
