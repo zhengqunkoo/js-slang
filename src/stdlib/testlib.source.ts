8f4652872b7a82f8c6835f41634725dcbd2b9874
Thomas Tan <829070+thomastanck@users.noreply.github.com>
2018-12-15 16:16:25 +0800

    Proof of concept of automatically including source libraries into Source spec

--- src/stdlib/testlib.source.ts
+++ src/stdlib/testlib.source.ts
@@ -1,6 +1,14 @@
 // ==== start of testlib ====
-function factorial(n: number): number {
+// factorial takes in a integer
+// and returns the factorial of that integer
+export function factorial(n: number): number {
   return n === 0 ? 1 : n * factorial(n - 1)
 }
+
+// sum_between takes in two integers
+// and returns the sum of all integers between them (inclusive)
+// using a closed form formula
+export function sum_between(start: number, end: number): number {
+  return ((start + end) * (start - end + 1)) / 2
+}
 // ==== end of testlib ====
-export const factorial1 = factorial
